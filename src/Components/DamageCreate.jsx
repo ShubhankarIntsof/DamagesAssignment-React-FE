c790c11532e5c77af945328373659c1bca959818
ShubhankarIntsof <shubhankar.shubham@intsof.com>
2022-10-28 12:12:41 +0530

    nested array problem solved

--- src/Components/DamageCreate.jsx
+++ src/Components/DamageCreate.jsx
@@ -1,15 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-function DamageCreate(props) {
+function DamageCreate() {
   const [evidencedata, setEData] = useState([]);
+  const [damageleveldata, setDamageData] = useState([]);
   const [evidencearray, setEArray] = useState([]);
+  const [damageEvidenceId, setDId] = useState([]);
+  const [damageByValue, setDValue] = useState([]);
+  var transferForLevel;
+
   const URLEvd =
     'https://localhost:44302/api/Damages/GetLkpDamageType';
+
+  const URLDamageLevel =
+    'https://localhost:44302/api/Damages/GetDamageEvidence/';
 
   useEffect(() => {
     fetchEvidenceData();
   }, []);
+
+  const fetchDamageLevel = (e) => {
+    fetch(URLDamageLevel + e.target.value)
+      .then((res) => res.json())
+
+      .then((response) => {
+        const r = JSON.stringify(response);
+        setDamageData(JSON.parse(r));
+        //var k = Object.keys(damageleveldata).length; //Object.keys(damageleveldata).length
+        var k = JSON.parse(r);
+        const value = e.target.value;
+        //const name = e.target.name;
+        //const checked = e.target.checked;
+
+        if (k == 0) {
+          console.log(k);
+          console.log('setting in array');
+          const finalvalue = {
+            damageType: value,
+            damageEvidence: null,
+            specifyOthers: null,
+            isDeleted: 'false',
+          };
+          setEArray([...evidencearray, finalvalue]);
+        } else {
+          console.log(k);
+          console.log('choose the level');
+          setDValue(value);
+          //transferForLevel = finalvalue;
+        }
+        console.log('Response DAMAGE LEVEL ' + r);
+      });
+  };
 
   const fetchEvidenceData = () => {
     fetch(URLEvd)
@@ -34,22 +75,38 @@ function DamageCreate(props) {
     };
     console.log(value, checked, name);
 
-    if (checked) {
-      setEArray([...evidencearray, finalvalue]);
+    if (checked == true) {
+      //fetchDamageLevel(value);
+      fetchDamageLevel(e);
     } else {
+      damageleveldata.splice(0, damageleveldata.length);
       setEArray(
         evidencearray.filter(
           (e) => e.damageType !== finalvalue.damageType
         )
       );
     }
-    console.log(JSON.stringify(finalvalue));
+  };
+
+  const handleDamageLevel = (e) => {
+    console.log(e.target.value);
+    //setDId(e.target.value);
+    var data = e.target.value;
+    const finalvalue = {
+      damageType: damageByValue,
+      damageEvidence: data,
+      specifyOthers: null,
+      isDeleted: 'false',
+    };
+    setEArray([...evidencearray, finalvalue]);
+    //transferForLevel.damageEvidence = e.target.value;
   };
 
   return (
     <div>
       <h3 style={{ marginTop: '40px' }}>New Damage Case</h3>
       {JSON.stringify(evidencearray)}
+      {'Damagelevel' + JSON.stringify(damageleveldata)}
       <form
         style={{
           display: 'flex',
@@ -109,6 +166,8 @@ function DamageCreate(props) {
       </form>
       <hr />
       <div>
+        <h6>Property Shows Damage By *</h6>
+        <div>{'  '} </div>
         {evidencedata.map((item, i) => (
           <div>
             {item.damageValue} &nbsp;
@@ -121,6 +180,23 @@ function DamageCreate(props) {
             />
           </div>
         ))}
+      </div>
+      <hr />
+      <div>
+        <h6>Evidence *</h6>
+        <form>
+          {damageleveldata.map((item, i) => (
+            <div>
+              {item.damageLevel + ' '}
+              <input
+                type="radio"
+                value={item.damageEvidenceId}
+                name="Evidence Level"
+                onChange={handleDamageLevel}
+              />
+            </div>
+          ))}
+        </form>
       </div>
     </div>
   );
