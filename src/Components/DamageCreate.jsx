49c9f2d47086611381a8105d9a6f494bce9a6001
ShubhankarIntsof <shubhankar.shubham@intsof.com>
2022-10-28 13:55:19 +0530

    create successful

--- src/Components/DamageCreate.jsx
+++ src/Components/DamageCreate.jsx
@@ -1,40 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 function DamageCreate() {
   const [evidencedata, setEData] = useState([]);
   const [damageleveldata, setDamageData] = useState([]);
   const [evidencearray, setEArray] = useState([]);
-  const [damageEvidenceId, setDId] = useState([]);
-  const [damageByValue, setDValue] = useState([]);
-  var transferForLevel;
+  const [damageByValue, setDValue] = useState([]); //stores damage value temporarily
+  const [damageBy, setDamageBy] = useState(); //using to set "Evidence of fire damage .."
+  const [estimate, setEstimate] = useState();
+  const [strDamage, setStrDamage] = useState();
+  const [sidingDamage, setSidingDamage] = useState();
+  const [roofMissing, setRoofMissing] = useState();
 
   const URLEvd =
     'https://localhost:44302/api/Damages/GetLkpDamageType';
 
   const URLDamageLevel =
-    'https://localhost:44302/api/Damages/GetDamageEvidence/';
+    'https://localhost:44302/api/Damages/GetDamageEvidence/'; //also a lookup table
+
+  const URLCreate =
+    'https://localhost:44302/api/Damages/PostMainData';
 
   useEffect(() => {
     fetchEvidenceData();
   }, []);
 
-  const fetchDamageLevel = (e) => {
+  const fetchDamageLevel = (e, n) => {
     fetch(URLDamageLevel + e.target.value)
       .then((res) => res.json())
 
       .then((response) => {
         const r = JSON.stringify(response);
         setDamageData(JSON.parse(r));
-        //var k = Object.keys(damageleveldata).length; //Object.keys(damageleveldata).length
         var k = JSON.parse(r);
         const value = e.target.value;
-        //const name = e.target.name;
-        //const checked = e.target.checked;
 
         if (k == 0) {
           console.log(k);
           console.log('setting in array');
+          setDamageBy();
           const finalvalue = {
             damageType: value,
             damageEvidence: null,
@@ -44,8 +48,9 @@ function DamageCreate() {
           setEArray([...evidencearray, finalvalue]);
         } else {
           console.log(k);
-          console.log('choose the level');
+          console.log(n);
           setDValue(value);
+          setDamageBy('Evidence of ' + n + ' Damage % *');
           //transferForLevel = finalvalue;
         }
         console.log('Response DAMAGE LEVEL ' + r);
@@ -63,6 +68,50 @@ function DamageCreate() {
       });
   };
 
+  const handleCreate = (e) => {
+    console.log(' Create Clicked ');
+    e.preventDefault();
+    try {
+      fetch(URLCreate, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          estimateOfDamages: estimate,
+          isStructuralDamage: strDamage,
+          isSlidingDamage: sidingDamage,
+          isRoofDamage: roofMissing,
+          shubhankarAssetInspectionEvidence: evidencearray,
+        }),
+      }).then((res) => {
+        console.log(res.status);
+        if (res.status == 200) {
+          window.location.reload();
+        } else {
+          alert('Something went wrong');
+        }
+      });
+    } catch (err) {
+      console.log('ERRR' + err);
+      alert(err);
+    }
+  };
+
+  const handleSidingDamage = (e) => {
+    console.log(' Siding Damage ' + e.target.value);
+    setSidingDamage(e.target.value);
+  };
+  const handleRoofDamage = (e) => {
+    console.log(' Roof Damage ' + e.target.value);
+    setRoofMissing(e.target.value);
+  };
+  const handleEstimateOfDamages = (e) => {
+    console.log(' Estimate of Damage ' + e.target.value);
+    setEstimate(e.target.value);
+  };
+  const handleStructuralDamage = (e) => {
+    console.log(' Structural Damage ' + e.target.value);
+    setStrDamage(e.target.value);
+  };
   const handleCheck = (e) => {
     const value = e.target.value;
     const name = e.target.name;
@@ -77,9 +126,10 @@ function DamageCreate() {
 
     if (checked == true) {
       //fetchDamageLevel(value);
-      fetchDamageLevel(e);
+      fetchDamageLevel(e, name);
     } else {
       damageleveldata.splice(0, damageleveldata.length);
+      setDamageBy();
       setEArray(
         evidencearray.filter(
           (e) => e.damageType !== finalvalue.damageType
@@ -99,14 +149,13 @@ function DamageCreate() {
       isDeleted: 'false',
     };
     setEArray([...evidencearray, finalvalue]);
-    //transferForLevel.damageEvidence = e.target.value;
   };
 
   return (
-    <div>
+    <div style={{ marginBottom: '40px' }}>
       <h3 style={{ marginTop: '40px' }}>New Damage Case</h3>
       {JSON.stringify(evidencearray)}
-      {'Damagelevel' + JSON.stringify(damageleveldata)}
+      {/* {'Damagelevel' + JSON.stringify(damageleveldata)} */}
       <form
         style={{
           display: 'flex',
@@ -129,6 +178,7 @@ function DamageCreate() {
             style={{ width: '250px', height: '30px' }}
             type="number"
             placeholder="Enter Estimate of Damages"
+            onChange={handleEstimateOfDamages}
           />
         </div>
 
@@ -138,6 +188,7 @@ function DamageCreate() {
             type="radio"
             value="Yes"
             name="Structural Damage"
+            onChange={handleStructuralDamage}
           />{' '}
           Yes
           <span> </span>
@@ -145,22 +196,47 @@ function DamageCreate() {
             type="radio"
             value="No"
             name="Structural Damage"
+            onChange={handleStructuralDamage}
           />{' '}
           No
         </div>
 
         <div style={{ marginTop: '10px' }}>
           <div>Siding Damage or Missing *</div>
-          <input type="radio" value="Yes" name="Siding Damage" /> Yes
+          <input
+            type="radio"
+            value="Yes"
+            name="Siding Damage"
+            onChange={handleSidingDamage}
+          />{' '}
+          Yes
           <span> </span>
-          <input type="radio" value="No" name="Siding Damage" /> No
+          <input
+            type="radio"
+            value="No"
+            name="Siding Damage"
+            onChange={handleSidingDamage}
+          />{' '}
+          No
         </div>
 
         <div style={{ marginTop: '10px' }}>
           <div>Roof Missing *</div>
-          <input type="radio" value="Yes" name="Roof Missing" /> Yes
+          <input
+            type="radio"
+            value="Yes"
+            name="Roof Missing"
+            onChange={handleRoofDamage}
+          />{' '}
+          Yes
           <span> </span>
-          <input type="radio" value="No" name="Roof Missing" /> No
+          <input
+            type="radio"
+            value="No"
+            name="Roof Missing"
+            onChange={handleRoofDamage}
+          />{' '}
+          No
         </div>
         <hr />
       </form>
@@ -183,7 +259,7 @@ function DamageCreate() {
       </div>
       <hr />
       <div>
-        <h6>Evidence *</h6>
+        <h6>{damageBy}</h6>
         <form>
           {damageleveldata.map((item, i) => (
             <div>
@@ -198,6 +274,15 @@ function DamageCreate() {
           ))}
         </form>
       </div>
+
+      <Button
+        style={{ marginTop: '15px' }}
+        variant="primary"
+        type="submit"
+        onClick={handleCreate}
+      >
+        Submit
+      </Button>
     </div>
   );
 }
